f936f003f4796cbfa00c9bfd7c1f7a17fc7c5d5e
Srosch23 <sroschblack@gmail.com>
2025-12-10 23:55:35 +0100

    Auto-sync from Orchids Desktop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        serif: ['Playfair Display', 'serif'],
+        serif: ['Cormorant Garamond', 'serif'],
         sans: ['Inter', 'sans-serif'],
       },
       colors: {
